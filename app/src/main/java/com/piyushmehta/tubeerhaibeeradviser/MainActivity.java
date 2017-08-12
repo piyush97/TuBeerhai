2c855d073afbb0035d1e772b67706e51ce970bba
piyush97 <me@piyushmehta.com>
2017-08-12 15:48:15 +0530

    new changes

--- app/src/main/java/com/piyushmehta/tubeerhaibeeradviser/MainActivity.java
+++ app/src/main/java/com/piyushmehta/tubeerhaibeeradviser/MainActivity.java
@@ -1,13 +1,32 @@
 package com.piyushmehta.tubeerhaibeeradviser;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //call when the user clicks the button
+
+    public void onClickFindBeer(View view) {
+        //get reference to the textview
+        TextView brands = (TextView) findViewById(R.id.brands);
+        brands.setText("Gottle Of Geer");
+        //reference to the spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //get the selected item
+        String beerType = String.valueOf(color.getSelectedItem());
+        //display the selected Item
+        brands.setText(beerType);
+
+
     }
 }
